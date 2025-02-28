5f7892bfb187d477d32f880d4dcc4445b656f22d
Brittani Lovelace <blovelace84@gmail.com>
2025-02-28 16:08:44 -0500

    New component to add
    BlackJackGame.jsx

--- Casino-App/src/App.jsx
+++ Casino-App/src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import PokerGame from "./Components/PokerGame";
+import BlackjackGame from "./Components/BlackJackGame";
 
 function App() {
   return (
    <div>
       <h1>Jackpot Royale</h1>
       <PokerGame />
+      <BlackjackGame />
    </div>
   )
 }
